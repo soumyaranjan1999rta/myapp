f2d289d7aacdd9fced1ab493e76f6e2353085047
Soumya Ranjan Mishra <79540393+soumya1999rta@users.noreply.github.com>
2021-11-19 21:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
-from flask import Flask,render_template,request
-import pickle
-import numpy as np
+from flask import Flask
 
-model = pickle.load(open('model.pkl','rb'))
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello World"
 
-@app.route('/predict',methods=['POST'])
-def predict_placement():
-    cgpa = float(request.form.get('cgpa'))
-    iq = int(request.form.get('iq'))
-    profile_score = int(request.form.get('profile_score'))
 
-    # prediction
-    result = model.predict(np.array([cgpa,iq,profile_score]).reshape(1,3))
-
-    if result[0] == 1:
-        result = 'placed'
-    else:
-        result = 'not placed'
-
-    return render_template('index.html',result=result)
 
 
 if __name__ == '__main__':
